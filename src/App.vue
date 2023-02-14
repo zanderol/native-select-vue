edaa228619c7ee38c80a621e01a09d8a51f4f5c5
ZanderOl <olexandruk@gmail.com>
2023-02-15 00:02:22 +0200

    create select options

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <template>
   <div id="app">
     <h1>Hello, Vue!</h1>
+    <v-select :options="options" @select="optionSelect" />
+    <p>Selected option goes here: {{ selected }}</p>
   </div>
 </template>
 
 <script>
+import vSelect from "./components/v-select.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    vSelect,
+  },
+  data() {
+    return {
+      options: [
+        { name: "Option 1", value: 1 },
+        { name: "Option 2", value: 2 },
+        { name: "Option 3", value: 3 },
+        { name: "Option 4", value: 4 },
+        { name: "Option 5", value: 5 },
+      ],
+      selected: "",
+    };
+  },
+  methods: {
+    optionSelect(option) {
+      this.selected = option.name;
+    },
+  },
 };
 </script>
 
---
+++ src/components/v-select.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="v-select">
+    <p class="title" @click="areOptionsVisible = !areOptionsVisible">Select</p>
+    <div class="options" v-if="areOptionsVisible">
+      <p
+        v-for="option in options"
+        :key="option.value"
+        @click="selectOption(option)"
+      >
+        {{ option.name }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-select",
+  props: {
+    options: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  data() {
+    return {
+      areOptionsVisible: false,
+    };
+  },
+  computed: {},
+  methods: {
+    selectOption(option) {
+      this.$emit("select", option);
+    },
+  },
+};
+</script>
+
+<style>
+.v-select {
+  position: relative;
+  width: 200px;
+  cursor: pointer;
+  user-select: none;
+}
+.title {
+  border: solid 1px #aeaeae;
+}
+.v-select p {
+  margin: 0;
+}
+.options {
+  border: solid 1px #aeaeae;
+  position: absolute;
+  top: 30px;
+  right: 0;
+  width: 100%;
+}
+.options p:hover {
+  background: #e7e7e7;
+}
+</style>
